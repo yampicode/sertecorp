672a62d3b5b4fc7f30c9df3b787b6cbf53c54911
Yampiero Mujica <jeanpiero01@hotmail.com>
2026-03-29 15:09:44 -0400

    Actualizar sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("/service-worker.js")
+      .register("/sw.js")
       .then(reg => console.log("✅ Service Worker registrado:", reg))
       .catch(err => console.error("❌ Error al registrar SW:", err));
   });
